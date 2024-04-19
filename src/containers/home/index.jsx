9d799020e12f26a71a37896218d50ea19df9541c
Sharukh khan <khansrk956@gmail.com>
2024-04-19 11:24:44 +0530

    home-button-done

--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import "./styles.scss";
+import { useNavigate } from "react-router-dom";
+import { Animate } from "react-simple-animate";
 
 const Home = () => {
+  const navigate = useNavigate();
+  const handleNavigateToContactMePage = () => {
+    navigate("/contact");
+  };
   return (
     <section id="home" className="home">
       <div className="home__text-wrapper">
@@ -11,6 +17,21 @@ const Home = () => {
           Front end developer
         </h1>
       </div>
+      <Animate
+        play
+        duration={1.5}
+        delay={1}
+        start={{
+          transform: "translateY(550px)",
+        }}
+        end={{
+          transform: "translatex(0px)",
+        }}
+      >
+        <div className="home__contact-me">
+          <button onClick={handleNavigateToContactMePage}>Hire Me</button>
+        </div>
+      </Animate>
     </section>
   );
 };
