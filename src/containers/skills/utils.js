cb85f3b52984e85321c645cac35f599f6ee55cc3
Sharukh khan <khansrk956@gmail.com>
2024-04-20 20:45:39 +0530

    skills completed

---
+++ src/containers/skills/utils.js
@@ -0,0 +1,26 @@
+export const skillsData = [
+  {
+    label: "FRONT END",
+    data: [
+      { skillName: "HTML", percentage: "90" },
+      { skillName: "CSS", percentage: "50" },
+      { skillName: "JAVASCRIPT", percentage: "80" },
+    ],
+  },
+  {
+    label: "BACK END",
+    data: [
+      { skillName: "JAVA", percentage: "65" },
+      { skillName: "PYTHON", percentage: "65" },
+      { skillName: "KOTLIN", percentage: "35" },
+    ],
+  },
+  {
+    label: "DATABASE",
+    data: [
+      { skillName: "SQL", percentage: "70" },
+      { skillName: "MONGO DB", percentage: "65" },
+      { skillName: "MYSQL", percentage: "35" },
+    ],
+  },
+];
