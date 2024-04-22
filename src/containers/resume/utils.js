f699b36277b8dd6756bbe32a23b87c2a87bda236
Sharukh khan <khansrk956@gmail.com>
2024-04-22 09:17:27 +0530

    resume completed

---
+++ src/containers/resume/utils.js
@@ -0,0 +1,42 @@
+export const data = {
+  experience: [
+    {
+      title: "Consultant",
+      subTitle: "Miami, FL",
+      description: "User Experience, Visual Design",
+    },
+    {
+      title: "Senior Consultant",
+      subTitle: "London, UK",
+      description: "Web Development, Databases, Backend Development",
+    },
+    {
+      title: "Project Manager",
+      subTitle: "Bangalore, India",
+      description: "Creative Direction project Managment, Team Leading",
+    },
+  ],
+
+  education: [
+    {
+      title: "Secondary ",
+      subTitle: "Sagar Vidya Niketan High School",
+      description: "10th Grade",
+    },
+    {
+      title: "Higher Senior Secondary ",
+      subTitle: "Aggarwal Sr. Sec. School",
+      description: "12th Grade in Science",
+    },
+    {
+      title: "Bsc",
+      subTitle: "Pt. J.L.N College, MDU Rohtak ",
+      description: "Bsc in Computer Science",
+    },
+    {
+      title: "MCA",
+      subTitle: "DAVIM College, MDU Rohtak",
+      description: "Master in Computer Application",
+    },
+  ],
+};
